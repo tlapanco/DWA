75f523fb45e9b987652f582cac359a4b53d89146
tl <99222342+tlapanco@users.noreply.github.com>
2025-07-12 04:17:34 +0000

    Add - Session 9

---
+++ Services/MongoDbService.cs
@@ -0,0 +1,21 @@
+using MongoDB.Driver;
+
+namespace DWA.Services
+{
+    public class MongoDbService
+    {
+        private readonly IMongoDatabase db;
+
+        public MongoDbService (IConfiguration configAppsettings)
+        {
+            var configMongo = configAppsettings.GetSection("MongoDB");
+            var clienteMongo = new MongoClient(configMongo["CadenaDeConexion"]);
+            db = clienteMongo.GetDatabase(configMongo["BaseDeDatos"]);
+        }
+
+        public IMongoCollection<TipoDocumento> ObtenerColeccion<TipoDocumento> (string nombreColeccion)
+        {
+            return db.GetCollection<TipoDocumento>(nombreColeccion);
+        }
+    }
+}
