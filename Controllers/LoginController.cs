7e54459599ec4570e4ff5e5194bb5657a4cf683e
tl <99222342+tlapanco@users.noreply.github.com>
2025-07-15 20:39:16 +0000

    Add - Session 11: Login files

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace DWA.Controllers   
+{
+    public class LoginController : Controller
+    {
+        // GET: LoginController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
