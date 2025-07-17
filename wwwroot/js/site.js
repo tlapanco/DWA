471f4938d89b4ce3d00ddaef428e131fdc6cabae
tl <99222342+tlapanco@users.noreply.github.com>
2025-07-17 18:46:41 +0000

    Fix - Assets paths for hosting

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,9 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+//Remove add from somee hosting
+document.addEventListener('DOMContentLoaded', () => {
+    const adds = document.getElementsByTagName('center');
+    adds[0].remove();
+})
